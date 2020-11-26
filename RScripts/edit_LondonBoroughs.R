9d6b9f3628abf153a2605d02f90cfb63562c1b78
vanessa.burton.sb <vanessa.burton.sb@VDI-SB-LR-2.ForestResearch.gov.uk>
2020-11-26 14:42:49 +0000

    add raw capital data to folder and R script for compiling/editing

--- RScripts/edit_LondonBoroughs.R
+++ RScripts/edit_LondonBoroughs.R
@@ -1,17 +1,85 @@
 
-wd <- "~/CRAFTY-opm"# sandbox VM
+library(tidyverse)
+library(raster)
+library(ggplot2)
+
+#wd <- "~/CRAFTY-opm"# sandbox VM
+wd <- "~/eclipse-workspace/CRAFTY_RangeshiftR"# sandbox VM
 dirOut <- file.path(wd, 'data-processed')
 
+
+# read in 'raw' data -----------------------------------------------------------
+
+# data produced in scripts:
+# OPM_nature_access.R
+# OPM_social_capitals.R
+# rangeshiftR-OPM_sensitivity.R
+
+OPMpresence <- read.csv(paste0(dirOut,"/capitals/hexG_rangeshiftR_test.csv"))
+social <- st_read(paste0(dirOut,"/capitals/hexG_social.shp"))
+natural <- st_read(paste0(dirOut,"/capitals/hexG_bio_access.shp"))
+
+social <- social %>% st_drop_geometry()
+capitals <- merge(natural,social,by="joinID")
+
+capitals <- capitals[,c(1,6,8,2,3)] %>% st_drop_geometry()
+
+capitals$OPMpresence <- OPMpresence$rep0_year9[match(capitals$joinID, OPMpresence$joinID)]
+
+capitals$riskPrc[which(is.na(capitals$riskPrc))] <- 0
+
+capitals$knowledge <- NA
+capitals$knowledge[which(capitals$OPMpresence>0)] <- 1
+capitals$knowledge[which(is.na(capitals$knowledge))] <- 0
+
+
+# normalise --------------------------------------------------------------------
+summary(capitals)
+# only need to normalise nature and OPM presence, rest are already 0-1
+
+# OPM presence
+data <- capitals$OPMpresence
+data[which(data==0)]<-NA
+normalised <- (data-min(data,na.rm = T))/(max(data, na.rm = T)-min(data,na.rm=T))
+hist(data)
+hist(normalised)
+normalised[which(is.na(normalised))]<-0
+capitals$OPMpresence <- normalised
+
+# nature
+data <- capitals$nature
+normalised <- (data-min(data,na.rm = T))/(max(data, na.rm = T)-min(data,na.rm=T))
+hist(data)
+hist(normalised)
+summary(normalised)
+normalised[which(is.na(normalised))]<-0
+capitals$nature <- normalised
+
+
+# join to CRAFTY coords ---------------------------------------------------------
+# original LondonBoroughs file and cellIDs for matching to CRAFTY coordinates
 london <- read_csv("~/eclipse-workspace/CRAFTY_RangeshiftR/data_LondonOPM/worlds/LondonBoroughs/LondonBoroughs_original.csv")
 hx <- read_csv("~/eclipse-workspace/CRAFTY_RangeshiftR/data-processed/Cell_ID_XY_Borough.csv")
-hexGrid <- read.csv(paste0(dirOut,"/hexGrids/hexG_rangeshiftR_test.csv"))
 
+head(london)
 colnames(london)[3:4] = c("Lon", "Lat")
 london$x  = hx$X[match(london$joinID, hx$Cell_ID)]
 london$y  = hx$Y[match(london$joinID, hx$Cell_ID)]
 
-london$OPMpresence <- hexGrid$rep0_year9[match(london$joinID, hexGrid$joinID)]
+head(london)
+london$OPMpresence <- capitals$OPMpresence[match(london$joinID, capitals$joinID)]
+london$riskPerc <- capitals$riskPrc[match(london$joinID, capitals$joinID)]
+london$budget <- capitals$budget[match(london$joinID, capitals$joinID)]
+london$OPMpresence <- capitals$OPMpresence[match(london$joinID, capitals$joinID)]
+london$knowledge <- capitals$knowledge[match(london$joinID, capitals$joinID)]
+london$nature <- capitals$nature[match(london$joinID, capitals$joinID)]
+london$access <- capitals$access[match(london$joinID, capitals$joinID)]
 
+# check
+ggplot(london)+
+  geom_tile(mapping = aes(x,y,fill=riskPerc))
+
+# tidy up
 london$id = NULL
 london$joinID = NULL
 london$Lon = NULL
@@ -20,32 +88,7 @@ london$FR = london$Agent
 london$Agent = NULL
 london$BT = 0
 
-# check
-ggplot(london)+
-  geom_tile(mapping = aes(x,y,fill=OPMpresence))
-
-summary(london)
-# normalise OPM presence 0-1
-data <- london$OPMpresence
-data[which(data==0)]<-NA
-normalised <- (data-min(data,na.rm = T))/(max(data, na.rm = T)-min(data,na.rm=T))
-hist(data)
-hist(normalised)
-normalised[which(is.na(normalised))]<-0
-london$OPMpresence <- normalised
-
-london$knowledge[which(london$OPMpresence>0)] <- 1
-
-# normalise nature
-data <- london$nature
-summary(data)
-data[which(data==0)]<-NA
-normalised <- (data-min(data,na.rm = T))/(max(data, na.rm = T)-min(data,na.rm=T))
-hist(data)
-hist(normalised)
-normalised[which(is.na(normalised))]<-0
-london$nature <- normalised
-
+head(london)
 summary(london)
 
 write.csv(london, "~/eclipse-workspace/CRAFTY_RangeshiftR/data_LondonOPM/worlds/LondonBoroughs/LondonBoroughs_XY.csv")
