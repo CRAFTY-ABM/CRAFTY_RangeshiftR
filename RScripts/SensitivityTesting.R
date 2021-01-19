dcd16ef235ecde10f2a7c96c5d819b3a2b6bb5f0
Vanessa Burton <vanessa.burton@forestresearch.gov.uk>
2021-01-19 11:59:51 +0000

    script for testing effect of capitals

---
+++ RScripts/SensitivityTesting.R
@@ -0,0 +1,16 @@
+
+# create files for sensitivity testing effect of social capitals
+
+# combinations:
+# A few simple scenarios - apply blanket across the area OR vary existing values to have high/low options
+# - High budget, high risk perception 
+# - Low budget, high risk perception 
+# - High budget, low risk perception
+# - Low budget, low risk perception
+
+# read in baseline
+
+baseline <- read.csv("~/eclipse-workspace/CRAFTY_RangeshiftR/data_LondonOPM/worlds/LondonBoroughs/LondonBoroughs_XY.csv")
+
+head(baseline)
+
