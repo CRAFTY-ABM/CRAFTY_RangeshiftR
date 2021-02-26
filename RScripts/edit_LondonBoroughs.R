5be012ac3e530014670ae0ab8c0e6bc8b6d8dad5
Vanessa Burton <vanessa.burton@forestresearch.gov.uk>
2021-02-26 11:05:14 +0000

    baseline normalised capitals

--- RScripts/edit_LondonBoroughs.R
+++ RScripts/edit_LondonBoroughs.R
@@ -12,7 +12,7 @@ scenario <- "de-regulation"
 
 # note. can skip to l.121 to read in baseline capitals and change from there.
 
-### read in 'raw' data -----------------------------------------------------------
+### read in raw data -----------------------------------------------------------
 
 # data produced in scripts:
 # OPM_nature_access.R
@@ -20,56 +20,48 @@ scenario <- "de-regulation"
 # rangeshiftR-OPM_sensitivity.R
 
 OPMpresence <- read.csv(paste0(dirOut,"/capitals/hexG_rangeshiftR_test.csv"))
-social <- st_read(paste0(dirOut,"/capitals/hexG_social.shp"))
-natural <- st_read(paste0(dirOut,"/capitals/hexG_bio_access.shp"))
+social <- st_read(paste0(dirOut,"/capitals/hexG_social_RAW.shp"))
+natural <- st_read(paste0(dirOut,"/capitals/hexG_bio_access_RAW.shp"))
 
 social <- social %>% st_drop_geometry()
-capitals <- merge(natural,social,by="joinID")
-head(capitals)
-capitals <- capitals[,c(1,6,8,2,3)] %>% st_drop_geometry()
+capitalsRAW <- merge(natural,social,by="joinID")
+head(capitalsRAW)
+capitalsRAW <- capitalsRAW[,c(1,3,4,7,9,10)] %>% st_drop_geometry()
 
-capitals$OPMpresence <- OPMpresence$rep0_year9[match(capitals$joinID, OPMpresence$joinID)]
+capitalsRAW$OPMpresence <- OPMpresence$rep0_year9[match(capitalsRAW$joinID, OPMpresence$joinID)]
 # test making OPM presence completely binary for CRAFTY
-capitals$OPMpresence[which(capitals$OPMpresence>0)]<-1
-unique(capitals$OPMpresence)
+capitalsRAW$OPMpresence[which(capitalsRAW$OPMpresence>0)]<-1
+unique(capitalsRAW$OPMpresence)
 
-capitals$riskPrc[which(is.na(capitals$riskPrc))] <- 0
+capitalsRAW$riskPerc[which(is.na(capitalsRAW$riskPerc))] <- 0
 
-capitals$knowledge <- NA
-capitals$knowledge[which(capitals$OPMpresence>0)] <- 1
-capitals$knowledge[which(is.na(capitals$knowledge))] <- 0
-
-head(capitals)
-
-# normalise --------------------------------------------------------------------
-summary(capitals)
-# only need to normalise nature, rest are already 0-1
-
-# commented out old code from when using OPM population values directly - don't need to do this now testing binary 0/1 OPM
-# add step here to normalised/standardise using max carrying capacity
-# otherwise normalising rangeshiftR populations in every run of CRAFTY_RangeshiftR will mess things up
-# OPM presence
-#data <- capitals$OPMpresence
-#data[which(data==0)]<-NA
-#normalised <- (data-min(data,na.rm = T))/(max(data, na.rm = T)-min(data,na.rm=T))
-#hist(data)
-#hist(normalised)
-#normalised[which(is.na(normalised))]<-0
-#capitals$OPMpresence <- normalised
+capitalsRAW$knowledge <- NA
+capitalsRAW$knowledge[which(capitalsRAW$OPMpresence>0)] <- 1
+capitalsRAW$knowledge[which(is.na(capitalsRAW$knowledge))] <- 0
 
 # inverted OPM presence capital 
-invert <- capitals$OPMpresence - 1
+invert <- capitalsRAW$OPMpresence - 1
 z <- abs(invert)
-capitals$OPMinv <- z
+capitalsRAW$OPMinv <- z
 
-# nature
-data <- capitals$nature
-normalised <- (data-min(data,na.rm = T))/(max(data, na.rm = T)-min(data,na.rm=T))
-hist(data)
-hist(normalised)
-summary(normalised)
-normalised[which(is.na(normalised))]<-0
-capitals$nature <- normalised
+head(capitalsRAW)
+summary(capitalsRAW)
+
+# normalise --------------------------------------------------------------------
+
+normalise <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+
+capitalsNORM <- data.frame(capitalsRAW[1], lapply(capitalsRAW[2:8], normalise))
+summary(capitalsNORM)
+
+capitalsNORM %>% 
+  pivot_longer(cols = nature:OPMinv, names_to = "capital", values_to = "value") %>% 
+  ggplot()+
+  geom_boxplot(aes(capital,value))
+
+write.csv(capitalsNORM, paste0(dirOut, "/capitals/baseline_capitals_norm.csv"),row.names = F)
 
 
 # join to CRAFTY coords ---------------------------------------------------------
@@ -88,18 +80,24 @@ lookUp <- london[,c(2,12,13)]
 #write.csv(lookUp,"~/eclipse-workspace/CRAFTY_RangeshiftR/data-processed/joinID_lookup.csv", row.names = F)
 
 head(london)
-london$OPMpresence <- capitals$OPMpresence[match(london$joinID, capitals$joinID)]
-london$OPMinverted <- capitals$OPMinv[match(london$joinID, capitals$joinID)]
-london$riskPerc <- capitals$riskPrc[match(london$joinID, capitals$joinID)]
-london$budget <- capitals$budget[match(london$joinID, capitals$joinID)]
-london$OPMpresence <- capitals$OPMpresence[match(london$joinID, capitals$joinID)]
-london$knowledge <- capitals$knowledge[match(london$joinID, capitals$joinID)]
-london$nature <- capitals$nature[match(london$joinID, capitals$joinID)]
-london$access <- capitals$access[match(london$joinID, capitals$joinID)]
+london$OPMpresence <- capitalsNORM$OPMpresence[match(london$joinID, capitalsNORM$joinID)]
+london$OPMinverted <- capitalsNORM$OPMinv[match(london$joinID, capitalsNORM$joinID)]
+london$riskPerc <- capitalsNORM$riskPerc[match(london$joinID, capitalsNORM$joinID)]
+london$budget <- capitalsNORM$budget[match(london$joinID, capitalsNORM$joinID)]
+london$OPMpresence <- capitalsNORM$OPMpresence[match(london$joinID, capitalsNORM$joinID)]
+london$knowledge <- capitalsNORM$knowledge[match(london$joinID, capitalsNORM$joinID)]
+london$nature <- capitalsNORM$nature[match(london$joinID, capitalsNORM$joinID)]
+london$access <- capitalsNORM$access[match(london$joinID, capitalsNORM$joinID)]
 
 # check
-ggplot(london)+
-  geom_tile(mapping = aes(x,y,fill=OPMinverted))
+london %>% 
+  pivot_longer(cols = c("OPMinverted","nature","access","riskPerc","budget","knowledge"), names_to = "capitals", values_to = "values") %>% 
+  ggplot()+
+  geom_tile(mapping = aes(x,y,fill=values))+
+  facet_wrap(~capitals)+
+  theme_bw()
+
+write.csv(london, paste0(dirOut,"/capitals/london_baseline_capitals_norm.csv"))
 
 ### baseline -------------------------------------------------------------------
 
@@ -113,7 +111,7 @@ london$Agent <- NULL
 london$BT <- 0
 
 head(london)
-london <- london[,c(7,8,1,9,2,3,4,5,6,10,11)]
+london <- london[,c(6,7,8,1,9,2,3,4,5,10,11)]
 summary(london)
 head(london)
 
@@ -124,19 +122,22 @@ write.csv(london, sprintf("~/eclipse-workspace/CRAFTY_RangeshiftR/data_LondonOPM
 
 ### de-regulation changes ------------------------------------------------------
 
-london <- read.csv("~/eclipse-workspace/CRAFTY_RangeshiftR/data_LondonOPM/worlds/LondonBoroughs/LondonBoroughs_original.csv")
+londonXY <- read.csv("~/eclipse-workspace/CRAFTY_RangeshiftR/data_LondonOPM/worlds/LondonBoroughs/LondonBoroughs_original.csv")
 hx <- read.csv("~/eclipse-workspace/CRAFTY_RangeshiftR/data-processed/Cell_ID_XY_Borough.csv")
-colnames(london)[3:4] = c("Lon", "Lat")
-london$x <- hx$X[match(london$joinID, hx$Cell_ID)]
-london$y <- hx$Y[match(london$joinID, hx$Cell_ID)]
-london$OPMpresence <- capitals$OPMpresence[match(london$joinID, capitals$joinID)]
-london$OPMinverted <- capitals$OPMinv[match(london$joinID, capitals$joinID)]
-london$riskPerc <- capitals$riskPrc[match(london$joinID, capitals$joinID)]
-london$budget <- capitals$budget[match(london$joinID, capitals$joinID)]
-london$OPMpresence <- capitals$OPMpresence[match(london$joinID, capitals$joinID)]
-london$knowledge <- capitals$knowledge[match(london$joinID, capitals$joinID)]
-london$nature <- capitals$nature[match(london$joinID, capitals$joinID)]
-london$access <- capitals$access[match(london$joinID, capitals$joinID)]
+capitalsNORM <- read.csv(paste0(dirOut, "/capitals/baseline_capitals_norm.csv"))
+colnames(londonXY)[3:4] = c("Lon", "Lat")
+londonXY$x <- hx$X[match(londonXY$joinID, hx$Cell_ID)]
+londonXY$y <- hx$Y[match(londonXY$joinID, hx$Cell_ID)]
+londonXY$OPMinverted <- capitalsNORM$OPMinv[match(londonXY$joinID, capitalsNORM$joinID)]
+londonXY$riskPerc <- capitalsNORM$riskPrc[match(londonXY$joinID, capitalsNORM$joinID)]
+londonXY$budget <- capitalsNORM$budget[match(londonXY$joinID, capitalsNORM$joinID)]
+londonXY$OPMpresence <- capitalsNORM$OPMpresence[match(londonXY$joinID, capitalsNORM$joinID)]
+londonXY$knowledge <- capitalsNORM$knowledge[match(londonXY$joinID, capitalsNORM$joinID)]
+londonXY$nature <- capitalsNORM$nature[match(londonXY$joinID, capitalsNORM$joinID)]
+londonXY$access <- capitalsNORM$access[match(londonXY$joinID, capitalsNORM$joinID)]
+
+head(londonXY)
+londonXY$Agent <- NULL
 
 # 1. define budget by each land owner type (replace simple borough levels atm)
 # need to come up with simple rules
@@ -144,19 +145,19 @@ london$access <- capitals$access[match(london$joinID, capitals$joinID)]
 # random scale for parks, 0.2, 0.6, 0.9
 # all other types medium 0.5?
 
-ggplot(london)+
+ggplot(londonXY)+
   geom_tile(mapping = aes(x,y,fill=budget))
 
 head(social)
-london$type <- social$type[match(london$joinID, social$joinID)]
-london$ownerID <- social$ownerID[match(london$joinID, social$joinID)]
+londonXY$type <- social$type[match(londonXY$joinID, social$joinID)]
+londonXY$ownerID <- social$ownerID[match(londonXY$joinID, social$joinID)]
 
-ggplot(london)+
+ggplot(londonXY)+
   geom_tile(mapping = aes(x,y,fill=type))
 
-head(london)
-unique(london$type)
-unique(london$ownerID[which(london$type=="Public.park")])
+head(londonXY)
+unique(londonXY$type)
+unique(londonXY$ownerID[which(londonXY$type=="Public.park")])
 
 # 2. increase risk perception through time (if it's low)
 # 3. stop updating knowledge based on OPM presence
